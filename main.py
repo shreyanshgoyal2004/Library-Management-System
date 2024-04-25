d41839dbe1b34616bf9bc5b8f04e564bffa479c9
shreyansh <shreyanshgoyal00@gmail.com>
2024-04-25 13:08:54 +0530

    Rate limiter functionality added

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
-from bson import ObjectId
-from fastapi import FastAPI, HTTPException, Query, status
+from fastapi import FastAPI, HTTPException, Header, Depends, status, Query
 from typing import Annotated
 from model.create_student import Item
 from model.update_student import updateItem
 from db import collection
+from datetime import datetime, timedelta
+from redis import Redis
+from bson.objectid import ObjectId
+from dotenv import dotenv_values
+import os
 
 app = FastAPI()
 
-# @app.get("/")
-# def home():
-#     return {"Hello world"}
+config = dotenv_values(os.getcwd() + "/.env")
 
+# Redis connection
+password = config['REDIS_PASSWORD']
+redis = Redis(host='redis-14380.c258.us-east-1-4.ec2.redns.redis-cloud.com', port=14380, password=password, db=0)
+print(redis.ping())
 
-# Define route to create an student
+# getting user_id from header
+def get_user_id(user_id: str = Header(None)):
+    if user_id:
+        return user_id
+    else:
+        raise HTTPException(status_code=400, detail="User-Id header is missing")
+
+# getting rate of that id if it present in redis db
+async def get_rate(redis: Redis, user_id: str):
+    return int(redis.get(f"rate_limit:{user_id}") or 0)
+
+# function to add new user with count 1
+async def add_rate(redis: Redis, user_id: str, count: int):
+    redis.set(f"rate_limit:{user_id}", count, ex=120)
+
+# function to increase count of the existing user
+async def update_rate(redis: Redis, user_id: str):
+    redis.incr(f"rate_limit:{user_id}")
+
+# async def check_rate_limit(x_user_id: str = Depends(get_user_id), rate: int = Depends(get_rate)) -> None:
+#     if rate >= 3:
+#         raise HTTPException(status_code=429, detail="Rate limit exceeded")
+
+@app.middleware("http")
+async def add_rate_limit_header(request, call_next):
+    user_id = request.headers.get("user_id", None)
+    if not redis.get(f"rate_limit:{user_id}"):
+        # rate = await get_rate(redis, user_id)
+        # await check_rate_limit(x_user_id=user_id, rate=rate)
+        await add_rate(redis, user_id,1)
+    else:
+        rate = await get_rate(redis, user_id)
+        await update_rate(redis,user_id)
+    response = await call_next(request)
+    return response
 
 @app.post("/students/", status_code=status.HTTP_201_CREATED, description= "API to create a student in the system. All fields are mandatory and required while creating the student in the system.")
 async def create_students(item: Item):
@@ -80,7 +120,7 @@ async def update_students(students_id: str, upd: updateItem|None = None):
         raise HTTPException(status_code=422, detail="Validation error")
     
 @app.delete("/students/{students_id}", status_code=status.HTTP_200_OK)
-def delete_students(students_id: str):
+async def delete_students(students_id: str):
     try:
         # Check if student exists
         student = collection.find_one({"_id": ObjectId(students_id)})
@@ -93,4 +133,3 @@ def delete_students(students_id: str):
         return {"message": "Student deleted successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
-
